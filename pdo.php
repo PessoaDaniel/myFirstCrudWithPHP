6d3c15056a098629bfe09888b8802684169ec9f4
Daniel <danielpessoa432@gmail.com>
2019-09-06 10:56:25 -0300

    teste

--- pdo.php
+++ pdo.php
@@ -1,14 +1,16 @@
 <?php
-$dsn = ('mysql: host=127.0.0.1; viewport=3306; dbname=projeto');
-$user = 'root';
-$pass = '';
-try{
-	$pdo = new PDO($dsn, $user, $pass);
-}
-catch(PDOException $ex){
-	echo 'Erro: ' .$ex-> getMessage();
-}
 
-
+function getConection(){
+	$dsn = ('mysql: host=127.0.0.1; viewport=3306; dbname=projeto; charset=utf8');
+	$user = 'root';
+	$pass = '1234';
+	try{
+		$pdo = new PDO($dsn, $user, $pass);
+		return $pdo;
+	}
+		catch(PDOException $ex){
+			echo 'Erro: ' .$ex-> getMessage();
+	}
+}  
 
 
