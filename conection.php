c123980507885fb51788aee6ae6353c148d0d579
Daniel <danielpessoa432@gmail.com>
2019-09-09 11:52:41 -0300

    trabalho-11:52

--- conection.php
+++ conection.php
@@ -4,15 +4,15 @@ include 'validacao.php';
 
     $con = getConection();
 
-   function  setPersonagem(array[]){
+   function  create(){
 
      $sql = 'insert into herois (nome, descricao,origem,sexo,raca,ranking) values (?,?,?,?,?,?)';
 
      $stmt = prepare($sql);
 
-     $stmt->bindValue(1, nome );
-     $stmt->bindValue(2, descpersona);
-     $stmt->bindValue(3, );
+     $stmt->bindValue(1, 'Zero_Two');
+     $stmt->bindValue(2, 'qualque coisa');
+     $stmt->bindValue(3, 'Desconhecida');
      $stmt->bindValue(4, 'F');
      $stmt->bindValue(5, 'Urrossauro');
      $stmt->bindValue(6, 'S');
@@ -21,8 +21,40 @@ include 'validacao.php';
    }
 
 
-   function getPersonagem(){
+    function read()
+    {
+        $sql = "SELECT * FROM personagens WHERE id = :id ";
+        $stmt = $con = getConection()->prepare($sql);
+        $stmt->bindValue(":id", 1);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+    }
 
+    function update(){
+       $sql = 'UPDATE peronagens SET nome =:dado WHERE id = :id';
 
-   }
+        $stmt = prepare($sql);
+
+        $stmt->bindValue(1, 'Naruto');
+
+    }
+
+    function delete(){
+
+        $con =  getConection();
+        $sql = 'DELETE FROM peronagens WHERE id = :id';
+        $stmt = $con->prepare($sql);
+
+        $stmt->bindValue(1, 1);
+    }
+
+/*$con = getConection();
+
+function  setPersonagem(array[]){
+
+    $sql = "insert into herois (nome, descricao,origem,sexo,raca,ranking) values ('02','qualquecoisa','desconhecida','F','Urrosauro','SS');";
+
+    $con-> exec($sql);
+*/
+
 
