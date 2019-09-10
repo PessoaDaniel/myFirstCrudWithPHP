c749f99a2c6ef4ecb19360df7667a02d116c07b1
Daniel <danielpessoa432@gmail.com>
2019-09-10 11:58:34 -0300

    trabalho-11:58

--- conection.php
+++ conection.php
@@ -2,10 +2,10 @@
  include 'pdo.php';
 
  $res=$pdo->prepare('INSERT INTO perssonagens(nome,anime,sexo,origem,ranking,descricaopersonagem) VALUES(:nom,:ani,:sex,:orig,:rank,:descricao)');
-$res->bindValue(":nom",'Zero_TWO');
-$res->bindValue(":ani",'Darling in the Franxx');
-$res->bindValue(":sex",'F');
-$res->bindValue(":orig",'Desconhecida');
-$res->bindValue(":rank",'S');
-$res->bindValue(":descricao",'Qualquer_coisa');
+$res->bindValue(":nom",'Naruto');
+$res->bindValue(":ani",'naruto');
+$res->bindValue(":sex",'M');
+$res->bindValue(":orig",'Konoha');
+$res->bindValue(":rank",'SS');
+$res->bindValue(":descricao",'Badass');
 $res->execute();
