115a60354dd7e25beba0107c89e2aaab66295ab5
Daniel <danielpessoa432@gmail.com>
2019-09-19 09:26:11 -0300

    aidção de pilhas

---
+++ javascript/pilha.php
@@ -0,0 +1,31 @@
+<?php
+
+class pilha
+{
+$topo;
+$base;
+    function _construct($i){
+        $pilha = array();
+    }
+    public function push(){
+        topo =
+    }
+    public function pop(){
+
+    }
+    public function isEmpty(){
+
+    }
+    public function isFull(){
+
+    }
+    public function consulta(){
+
+    }
+}
+
+class main
+{
+
+
+}
